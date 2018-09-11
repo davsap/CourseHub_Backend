53aaf6f023523cc06923db1a6a481c714cc2e52a
JulieB47 <julie.bergagnini@gmail.com>
2018-09-11 10:17:59 +0200

    MaJ Requete create user

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -4,8 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 
-
-
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -14,79 +12,77 @@ import model.Formation;
 import model.User;
 
 public class UserDAO {
-	
-	public static Stream<User> list(){
+
+	public static Stream<User> list() {
 		Session s = HibernateHelper.getSessionFactory().openSession();
-		Transaction t= null;
+		Transaction t = null;
 		Stream<User> result = null;
-		
-	try {
-		t = s.beginTransaction();
-		//code
-		result = s.createQuery("FROM utilisateur").list().stream();
-		
-		t.commit();
-		
-	}catch(Exception e) {
-		if(t !=null)
-			t.rollback();
 
-	} finally {
+		try {
+			t = s.beginTransaction();
+			// code
+			result = s.createQuery("FROM Utilisateurs").list().stream();
+			t.commit();
 
-		s.close();
-
-	}
-	return result;
-	}
-	
-public static User create(User u) {
-	
-	Session s = HibernateHelper.getSessionFactory().openSession();
-	Transaction t= null;
-	
-	
-	try {
-	t = s.beginTransaction();
-	//code
-	s.save(u);
-	
-	t.commit();
-	
-		}catch(Exception e) {
-			if(t !=null)
-		t.rollback();
+		} catch (Exception e) {
+			if (t != null)
+				t.rollback();
 
 		} finally {
 
 			s.close();
 
 		}
-return u;
+		return result;
+	}
 
-}
-public static User delete(User u) {
-	
-	Session s = HibernateHelper.getSessionFactory().openSession();
-	Transaction t= null;
-	
-	
-	try {
-	t = s.beginTransaction();
-	//code
-	s.delete(u);
-	
-	t.commit();
-	
-		}catch(Exception e) {
-			if(t !=null)
-		t.rollback();
+	public static User create(User u) {
+
+		Session s = HibernateHelper.getSessionFactory().openSession();
+		Transaction t = null;
+
+		try {
+			t = s.beginTransaction();
+			// code
+			s.save(u);
+
+			t.commit();
+
+		} catch (Exception e) {
+			if (t != null)
+				t.rollback();
 
 		} finally {
 
 			s.close();
 
 		}
-return u;
+		return u;
 
-}
+	}
+
+	public static User delete(User u) {
+
+		Session s = HibernateHelper.getSessionFactory().openSession();
+		Transaction t = null;
+
+		try {
+			t = s.beginTransaction();
+			// code
+			s.delete(u);
+
+			t.commit();
+
+		} catch (Exception e) {
+			if (t != null)
+				t.rollback();
+
+		} finally {
+
+			s.close();
+
+		}
+		return u;
+
+	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -16,6 +16,88 @@ import javax.persistence.Table;
 @Table(name="Utilisateurs")
 public class User {
 	
+	public User() {}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	@Column(name="id_utilisateur")
@@ -47,96 +129,6 @@ public class User {
 	
 	@Column(name="pays")
 	private String pays;
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getPseudo() {
-		return pseudo;
-	}
-
-	public void setPseudo(String pseudo) {
-		this.pseudo = pseudo;
-	}
-
-	public String getSituation() {
-		return situation;
-	}
-
-	public void setSituation(String situation) {
-		this.situation = situation;
-	}
-
-	public String getVille() {
-		return ville;
-	}
-
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	public String getPays() {
-		return pays;
-	}
-
-	public void setPays(String pays) {
-		this.pays = pays;
-	}
-	
-	/*public User() {
-		this.formations=new HashSet<>();
-	}*/
-	
-	//private Set<Formation> formations;
-	/* public void addFormation (Formation f) {
-	
-	formations.add(f);
-	
-	} */
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	@Override
 	public String toString() {
