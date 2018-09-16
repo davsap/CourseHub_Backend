df5ab794f66f18295ffdcef51912ab6be4399ed6
davsap <david.parto@hotmail.com>
2018-09-16 05:09:48 +0200

    problem de ordre sur sujet et cours est resolu

--- src/model/Sujet.java
+++ src/model/Sujet.java
@@ -89,5 +89,29 @@ public class Sujet implements Comparable<Sujet> {
 	    public int compareTo(Sujet sujet) {
 	        return (int) (this.getId() - sujet.getId());
 	    }
+	@Override
+	public int hashCode() {
+		final int prime = 17;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sujet other = (Sujet) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
+	 
 }
